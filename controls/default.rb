8a9104ea85db385f2e6b09cc17c8b004ddad241a
akshayparvatikar29 <167433616+akshayparvatikar29@users.noreply.github.com>
2024-08-04 22:51:18 +0530

    Delete controls/default.rb

--- controls/default.rb
+++
@@ -1,8 +0,0 @@
-control 'file-test' do
-  impact 1.0
-  title 'Check Certificate'
-  desc 'Check for the version'
-  describe x509_certificate(filepath: '/home/ec2-user/certificates/oldcert.crt') do
-    its('validity_in_days') { should be > 30 }
-  end
-end
